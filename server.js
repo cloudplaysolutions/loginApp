a37a190192dfabce5eeb523558be78043556f4ab
cloudplaysolutions <166919227+cloudplaysolutions@users.noreply.github.com>
2024-12-16 19:12:43 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+const routes = require('./routes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api', routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
